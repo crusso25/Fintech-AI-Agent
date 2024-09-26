868658761ff5aca468fb36cc3e3afca5b47a44a3
crusso25 <crusso25@vt.edu>
2024-09-25 20:28:22 -0400

    update init.bat

--- init.bat
+++ init.bat
@@ -20,7 +20,7 @@ start cmd /k "uvicorn main:app --reload --port 8002"
 :: Start the main API server
 echo Starting Transaction Simulator API on port 8003...
 cd ../fraud_detection
-start cmd /k "transaction_simulator_api:app --reload --port 8003"
+start cmd /k "uvicorn transaction_simulator_api:app --reload --port 8003"
 
 :: Start Dashboard
 echo Starting Dashboard with npm...
